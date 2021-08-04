f3e85cffccf95b4f6f45142f6b069773cc133424
hayeon17kim <hayeon17kim@gmail.com>
2021-08-05 02:00:57 +0900

    add : roomContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
-import Room from './components/Room';
+import RoomContainer from './components/RoomContainer';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} exact />
-      <Route path="/room" component={Room} exact />
-      <Route path="/room:roomId" component={Room} exact />
+      <Route path="/room" component={RoomContainer} exact />
+      <Route path="/room:roomId" component={RoomContainer} exact />
     </Switch>
   </BrowserRouter>
 );
