a2eac7781ed933104667c69839b142fd95b1a75d
hayeon17kim <hayeon17kim@gmail.com>
2021-08-09 03:29:42 +0900

    add : localStream, roomId state

---
+++ src/state/room.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { atom, selectorFamily } from 'recoil';
+
+export const getRoomId = selectorFamily({
+  key: 'getRoomId',
+  get: () => async () => {
+    const url = 'http://localhost:3000';
+    const response = await axios.get(`${url}/room/`);
+    return response.data;
+  },
+});
+
+export const localStream = atom({
+  key: 'localStream',
+  default: null,
+});
+
+export const localIsOpen = atom({
+  key: 'localIsOpen',
+  default: true,
+});
