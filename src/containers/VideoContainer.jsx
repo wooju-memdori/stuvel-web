3335b6173c9db8f7bb5a9dfd121ba5a61b5e770f
Hayeon Kim <hayeon17kim@gmail.com>
2021-08-15 20:26:50 +0900

    add : stream socket emitter

--- src/containers/VideoContainer.jsx
+++ src/containers/VideoContainer.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import { useRecoilState } from 'recoil';
+import React, { useState } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import VideoItem from '../components/Video';
-import { localStream } from '../state/atom';
+import {
+  userState,
+  localStream,
+  peersInfoState,
+  usersState,
+} from '../state/atom';
+import { getPeer, getSocket } from '../ws';
 
-const VideoContainer = () => {
+const VideoContainer = ({ roomId }) => {
   const [localStreamValue, setLocalStream] = useRecoilState(localStream);
+  const [peersInfo, setPeersInfo] = useRecoilState(peersInfoState);
+  const [users, setUsers] = useRecoilState(usersState);
+  const user = useRecoilValue(userState);
 
   function closeStream() {
     setLocalStream(null);
@@ -15,12 +24,32 @@ const VideoContainer = () => {
   }
 
   function openStream() {
+    const myPeer = getPeer();
+    const socket = getSocket();
+
     navigator.mediaDevices
       .getUserMedia({
         video: true,
       })
       .then((stream) => {
         setLocalStream(stream);
+        console.log(user);
+        socket.emit('stream-opened', roomId, user);
+
+        for (let otherId in users) {
+          myPeer.call(otherId, stream);
+          console.log('called');
+          /*
+          let call = peersInfoState[otherId]
+            ? peersInfoState[otherId].call
+            : null;
+          if (call) {
+            call.answer(stream);
+            console.log('answered');
+          } else {
+          }
+          */
+        }
       });
   }
 
@@ -32,7 +61,18 @@ const VideoContainer = () => {
       >
         카메라 키기
       </button>
-      <VideoItem stream={localStreamValue} />
+      {localStreamValue ? (
+        <VideoItem stream={localStreamValue} />
+      ) : (
+        <div>카메라 꺼짐</div>
+      )}
+      {Object.keys(peersInfo).map((otherId) =>
+        peersInfo[otherId] ? (
+          <VideoItem key={otherId} stream={peersInfo[otherId].stream} />
+        ) : (
+          <div>{otherId}는 stream이 없습니다.</div>
+        ),
+      )}
     </div>
   );
 };
