cdeaf3885678a172b3e23a36a362be6a2e273b83
Hayeon Kim <hayeon17kim@gmail.com>
2021-08-16 14:17:43 +0900

    add : #4 invitation link copy button

--- src/components/FootBar.jsx
+++ src/components/FootBar.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Button } from 'antd';
+import React, { useState } from 'react';
+import { Layout, Button, message } from 'antd';
 import { useRecoilValue } from 'recoil';
 import {
   camStatusState,
@@ -7,13 +7,20 @@ import {
   displayStreamState,
 } from '../state/atom';
 
-const FootBar = ({ handleMyCam, handleMyMic, toggleScreenShare }) => {
+const FootBar = ({ handleMyCam, handleMyMic, toggleScreenShare, roomId }) => {
   const camStatus = useRecoilValue(camStatusState);
   const micStatus = useRecoilValue(micStatusState);
   const displayStream = useRecoilValue(displayStreamState);
 
+  const copy = () => {
+    navigator.clipboard
+      .writeText(`http://localhost:3002/room/${roomId}`)
+      .then(() => message.success('Invitation link copied!'))
+      .catch(() => message.error('Browser not support clipboard api'));
+  };
+
   return (
-    <>
+    <Layout.Footer>
       <Button onClick={handleMyCam}>
         {camStatus ? 'Disable Cam' : 'Enable Cam'}
       </Button>
@@ -23,8 +30,8 @@ const FootBar = ({ handleMyCam, handleMyMic, toggleScreenShare }) => {
       <Button onClick={toggleScreenShare}>
         {displayStream ? 'Stop Screen Share' : 'Share Screen'}
       </Button>
-      {camStatus ? <div>your cam is on</div> : <div>your cam is off</div>}
-    </>
+      <Button onClick={copy}>Invite Link</Button>
+    </Layout.Footer>
   );
 };
 
