6b43c0aebd4e824481bb48236a16c7d51aa56d98
Hayeon Kim <hayeon17kim@gmail.com>
2021-08-20 01:31:12 +0900

    add : sidebar to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,13 @@ import { Layout, Menu, Button } from 'antd';
 import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
 import { collapsedState } from '../state/atom';
+import {
+  LogoIcon,
+  PlanetIcon,
+  ChatIcon,
+  SettingsIcon,
+  FriendsIcon,
+} from './Icon';
 
 const { Header, Content, Sider } = Layout;
 
@@ -27,8 +34,21 @@ const HomePage = () => {
       <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
         <div>로고자리</div>
         <Menu theme="dark" mode="inline">
-          <Menu.Item key="1">메뉴아이템1</Menu.Item>
-          <Menu.Item key="2">메뉴아이템2</Menu.Item>
+          <Menu.Item key="1" icon={<LogoIcon />}>
+            <a href="/">STUVEL</a>
+          </Menu.Item>
+          <Menu.Item href="/room" key="2" icon={<PlanetIcon />}>
+            <a href="/room">행성탐사</a>
+          </Menu.Item>
+          <Menu.Item key="3" icon={<ChatIcon />}>
+            채팅
+          </Menu.Item>
+          <Menu.Item key="4" icon={<SettingsIcon />}>
+            설정
+          </Menu.Item>
+          <Menu.Item key="5" icon={<FriendsIcon />}>
+            동료
+          </Menu.Item>
         </Menu>
       </Sider>
 
