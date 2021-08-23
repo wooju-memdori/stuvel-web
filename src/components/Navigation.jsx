f46054ff9057fca7d558ff4688f6bed3db5341ac
Hayeon Kim <hayeon17kim@gmail.com>
2021-08-23 13:11:18 +0900

    add : friends modal

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { func } from 'prop-types';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { Layout, Menu } from 'antd';
-import { collapsedState } from '../state/atom';
+import { collapsedState, isModalVisibleState } from '../state/atom';
+import Friends from './Friends';
 import {
   LogoIcon,
   PlanetIcon,
@@ -13,28 +14,36 @@ import {
 
 const Navigation = ({ onCollapse }) => {
   const collapsed = useRecoilValue(collapsedState);
+  const setIsModalVisible = useSetRecoilState(isModalVisibleState);
+  const showModal = () => {
+    setIsModalVisible(true);
+    console.log(true);
+  };
 
   return (
-    <Layout.Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
-      <div>로고자리</div>
-      <Menu theme="dark" mode="inline">
-        <Menu.Item key="1" icon={<LogoIcon />}>
-          <a href="/">STUVEL</a>
-        </Menu.Item>
-        <Menu.Item href="/room" key="2" icon={<PlanetIcon />}>
-          <a href="/room">행성탐사</a>
-        </Menu.Item>
-        <Menu.Item key="3" icon={<ChatIcon />}>
-          채팅
-        </Menu.Item>
-        <Menu.Item key="4" icon={<SettingsIcon />}>
-          설정
-        </Menu.Item>
-        <Menu.Item key="5" icon={<FriendsIcon />}>
-          동료
-        </Menu.Item>
-      </Menu>
-    </Layout.Sider>
+    <>
+      <Friends />
+      <Layout.Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+        <div>로고자리</div>
+        <Menu theme="dark" mode="inline">
+          <Menu.Item key="1" icon={<LogoIcon />}>
+            <a href="/">STUVEL</a>
+          </Menu.Item>
+          <Menu.Item href="/room" key="2" icon={<PlanetIcon />}>
+            <a href="/room">행성탐사</a>
+          </Menu.Item>
+          <Menu.Item key="3" icon={<ChatIcon />}>
+            채팅
+          </Menu.Item>
+          <Menu.Item key="4" icon={<SettingsIcon />}>
+            설정
+          </Menu.Item>
+          <Menu.Item onClick={showModal} key="5" icon={<FriendsIcon />}>
+            동료
+          </Menu.Item>
+        </Menu>
+      </Layout.Sider>
+    </>
   );
 };
 
