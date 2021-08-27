af6e84650d39614d88e08f896672e1a9fd6fe1f6
Hee-jin506 <59636609+Hee-jin506@users.noreply.github.com>
2021-08-27 14:13:31 +0900

    add : show nav bar everywhere

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,13 @@
 import React from 'react';
-import { Layout, Button, Row, Col } from 'antd';
-import { useRecoilState } from 'recoil';
-import { collapsedState } from '../state/atom';
-import Navigation from './Navigation';
+import { Button, Col } from 'antd';
 
 const HomePage = () => {
-  const [collapsed, setCollapsed] = useRecoilState(collapsedState);
-
-  const onCollapse = () => {
-    setCollapsed(!collapsed);
-  };
-
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Navigation onCollapse={onCollapse} collapsed={collapsed} />
-      <Layout className="site-layout">
-        <Layout.Content style={{ margin: '0 16px' }}>
-          <Row justify="center" align="middle">
-            <Col span={4}>
-              <Button href="/room" type="primary" shape="round" centered="true">
-                랜덤으로 출발하기
-              </Button>
-            </Col>
-          </Row>
-        </Layout.Content>
-      </Layout>
-    </Layout>
+    <Col span={4}>
+      <Button href="/room" type="primary" shape="round" centered="true">
+        랜덤으로 출발하기
+      </Button>
+    </Col>
   );
 };
 
