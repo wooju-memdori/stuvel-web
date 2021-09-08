b1c9433a67c466b8543e958a02e9cb3563c07bb6
Hayeon Kim <hayeon17kim@gmail.com>
2021-09-08 22:28:44 +0900

    chat message input

---
+++ src/components/Chat/Input.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Input = ({ setMessage, sendMessage, message }) => (
+  <>
+    <Form>
+      <StyledInput
+        type="text"
+        placeholder="Type a message..."
+        value={message}
+        onChange={({ target: { value } }) => setMessage(value)}
+        onKeyPress={(event) =>
+          event.key === 'Enter' ? sendMessage(event) : null
+        }
+      />
+      <SendButton onClick={(e) => sendMessage(e)}>Send</SendButton>
+    </Form>
+  </>
+);
+
+export default Input;
+
+const Form = styled.form`
+  display: flex;
+  border-top: 2px solid #d3d3d3;
+`;
+
+const StyledInput = styled.textarea`
+  border: none;
+  border-radius: 0;
+  padding: 5%;
+  width: 80%;
+  background-color: #15002a !important;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const SendButton = styled.button`
+  color: #fff !important;
+  text-transform: uppercase;
+  text-decoration: none;
+  background: #290054;
+  padding: 20px;
+  display: inline-block;
+  border: none;
+  width: 20%;
+`;
