4c878c6a8475642ddc1cd1880f42a0ad768e4c76
Hayeon Kim <hayeon17kim@gmail.com>
2021-09-09 00:38:43 +0900

    add : scrolling to chat

--- src/components/Chat/Messages.jsx
+++ src/components/Chat/Messages.jsx
@@ -1,13 +1,25 @@
 /*eslint-disable*/
 import React from 'react';
 
+import ScrollToBottom from 'react-scroll-to-bottom';
+import styled from 'styled-components';
+
 import Message from './Message';
 
-const Messages = ({ messages, name }) =>
-  messages.map((message, i) => (
-    <div key={i}>
-      <Message message={message} name={name} />
-    </div>
-  ));
+const Messages = ({ messages, name }) => (
+  <Scroll>
+    {messages.map((message, i) => (
+      <div key={i}>
+        <Message message={message} name={name} />
+      </div>
+    ))}
+  </Scroll>
+);
+
+const Scroll = styled(ScrollToBottom)`
+  padding: 5% 0;
+  overflow: auto;
+  flex: auto;
+`;
 
 export default Messages;
