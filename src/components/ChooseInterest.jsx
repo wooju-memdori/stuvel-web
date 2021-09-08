6e2c76741d29e83901adfb5f4b9f315b8512ed9c
TaeheeKim15 <utility_maker@naver.com>
2021-09-08 21:35:43 +0900

    add tag data

--- src/components/ChooseInterest.jsx
+++ src/components/ChooseInterest.jsx
@@ -32,82 +32,144 @@ const ChooseInterest = () => {
     addInterests();
   };
 
-  const world = [
-    { label: 'economy', value: 'economy' },
-    { label: 'market', value: 'market' },
-    { label: 'social issue', value: 'social issue' },
-    { label: 'politics', value: 'politics' },
+  const Knowledge = [
+    { label: 'History', value: '1' },
+    { label: 'Science', value: '2' },
+    { label: 'Physics', value: '3' },
+    { label: 'Math', value: '4' },
+    { label: 'Psychology', value: '5' },
+    { label: 'The Future', value: '6' },
+    { label: 'Education', value: '7' },
+    { label: 'Philosophy', value: '8' },
+    { label: 'Space', value: '9' },
+    { label: 'Covid-19', value: '10' },
+    { label: 'Biology', value: '11' },
   ];
 
-  const sports = [
-    { label: 'baseball', value: 'baseball' },
-    { label: 'basketball', value: 'basketball' },
-    { label: 'golf', value: 'golf' },
-    { label: 'soccer', value: 'soccer' },
-    { label: 'tennis', value: 'tennis' },
-    { label: 'vollyball', value: 'vollyball' },
+  const Wellness = [
+    { label: 'Weights', value: '12' },
+    { label: 'Veganism', value: '13' },
+    { label: 'Health', value: '14' },
+    { label: 'Meditation', value: '15' },
+    { label: 'Fitness', value: '16' },
   ];
 
-  const tech = [
-    { label: 'engineering', value: 'engineering' },
-    { label: 'AI', value: 'AI' },
-    { label: 'developer', value: 'developer' },
-    { label: 'designer', value: 'designer' },
-    { label: 'VR/AR', value: 'VR/AR' },
+  const Tech = [
+    { label: 'Venture', value: '17' },
+    { label: 'Startups', value: '18' },
+    { label: 'Product', value: '19' },
+    { label: 'Engineering', value: '20' },
+    { label: 'VR/AR', value: '21' },
+    { label: 'AI', value: '22' },
+    { label: 'Marketing', value: '23' },
+    { label: 'Crypto', value: '24' },
+    { label: 'Programmer', value: '25' },
+    { label: 'Designer', value: '26' },
+    { label: 'Project Manager', value: '27' },
   ];
 
-  const art = [
-    { label: 'food', value: 'Apple' },
-    { label: 'dance', value: 'Travel' },
-    { label: 'fashion', value: 'Marriage' },
-    { label: 'beauty', value: 'beauty' },
-    { label: 'design', value: 'design' },
-    { label: 'book', value: 'book' },
-    { label: 'advertisement', value: 'advertisement' },
-    { label: 'photo', value: 'photo' },
+  const Languages = [
+    { label: 'Portuguese', value: '28' },
+    { label: 'Indonesian', value: '29' },
+    { label: 'German', value: '30' },
+    { label: 'Hindi', value: '31' },
+    { label: 'Mandarin', value: '32' },
+    { label: 'Spanish', value: '33' },
+    { label: 'Japanese', value: '34' },
+    { label: 'Arabic', value: '35' },
+    { label: 'Russian', value: '36' },
+    { label: 'French', value: '37' },
+    { label: 'English', value: '38' },
+    { label: 'Korean', value: '39' },
   ];
 
-  const entertainment = [
-    { label: 'game', value: 'game' },
-    { label: 'TV', value: 'TV' },
-    { label: 'comedy', value: 'comedy' },
-    { label: 'story', value: 'story' },
-    { label: 'music', value: 'music' },
-    { label: 'movie', value: 'movie' },
-    { label: 'comics', value: 'comics' },
-    { label: 'cartoon', value: 'cartoon' },
+  const Entertainment = [
+    { label: 'Gaming', value: '40' },
+    { label: 'Music', value: '41' },
+    { label: 'Karaoke', value: '42' },
+    { label: 'Celebrities', value: '43' },
+    { label: 'Podcasts', value: '44' },
+    { label: 'Anime & Manga', value: '45' },
+    { label: 'Fun', value: '46' },
+    { label: 'Variety', value: '47' },
+    { label: 'Movies', value: '48' },
+    { label: 'Television', value: '49' },
+    { label: 'Performances', value: '50' },
+    { label: 'Comedy', value: '51' },
+    { label: 'Storytelling', value: '52' },
   ];
 
-  const language = [
-    { label: 'Portuguese', value: 'Portuguese' },
-    { label: 'Indonesian', value: 'Indonesian' },
-    { label: 'German', value: 'German' },
-    { label: 'Chinese', value: 'Chinese' },
-    { label: 'hindi', value: 'hindi' },
-    { label: 'Spanish', value: 'Spanish' },
-    { label: 'Japanese', value: 'Japanese' },
-    { label: 'Russian', value: 'Russian' },
-    { label: 'English', value: 'English' },
-    { label: 'Korean', value: 'Korean' },
+  const Life = [
+    { label: 'Pregnancy', value: '53' },
+    { label: 'Travelling', value: '54' },
+    { label: 'Weddings', value: '55' },
+    { label: 'Dating', value: '56' },
+    { label: 'Parenting', value: '57' },
+    { label: 'Relationships', value: '58' },
+    { label: 'Job Hunting', value: '59' },
+    { label: 'Career', value: '60' },
+    { label: 'Real Estate', value: '61' },
+    { label: 'Stocks', value: '62' },
+    { label: 'Side Jobs', value: '63' },
+    { label: 'Networking', value: '64' },
+    { label: 'Business', value: '65' },
+    { label: 'Entrepreneurship', value: '66' },
+  ];
+
+  const Sports = [
+    { label: 'Basketball', value: '67' },
+    { label: 'MMA', value: '68' },
+    { label: 'Golf', value: '69' },
+    { label: 'Soccer', value: '70' },
+    { label: 'Football', value: '71' },
+    { label: 'Cycling', value: '72' },
+    { label: 'Baseball', value: '73' },
+    { label: 'Tennis', value: '74' },
+    { label: 'Volleyball', value: '75' },
+  ];
+
+  const Arts = [
+    { label: 'Food & Drink', value: '76' },
+    { label: 'Dance', value: '77' },
+    { label: 'Theater', value: '78' },
+    { label: 'Fashion', value: '79' },
+    { label: 'Beauty', value: '80' },
+    { label: 'Design', value: '81' },
+    { label: 'Sci-Fi', value: '82' },
+    { label: 'Writing', value: '83' },
+    { label: 'Photography', value: '84' },
+    { label: 'Advertising', value: '85' },
+    { label: 'Architecture', value: '86' },
+    { label: 'Books', value: '87' },
+  ];
+
+  const WorldAffairs = [
+    { label: 'Economics', value: '88' },
+    { label: 'Markets', value: '89' },
+    { label: 'Social Issues', value: '90' },
+    { label: 'Current Events', value: '91' },
+    { label: 'Climate', value: '92' },
+    { label: 'Politics', value: '93' },
+    { label: 'Geopolitics', value: '94' },
   ];
 
   const MBTI = [
-    { label: 'ISTJ', value: 'ISTJ' },
-    { label: 'ISTP', value: 'ISTP' },
-    { label: 'ISFJ', value: 'ISFJ' },
-    { label: 'ISFP', value: 'ISFP' },
-    { label: 'INTJ', value: 'INTJ' },
-    { label: 'INTP', value: 'INTP' },
-    { label: 'INFJ', value: 'INFJ' },
-    { label: 'INFP', value: 'INFP' },
-    { label: 'ESTJ', value: 'ESTJ' },
-    { label: 'ESTP', value: 'ESTP' },
-    { label: 'ESFJ', value: 'ESFJ' },
-    { label: 'ESFP', value: 'ESFP' },
-    { label: 'ENTJ', value: 'ENTJ' },
-    { label: 'ENTP', value: 'ENTP' },
-    { label: 'ENFJ', value: 'ENFJ' },
-    { label: 'ENFP', value: 'ENFP' },
+    { label: 'ISTJ', value: '95' },
+    { label: 'ISTP', value: '96' },
+    { label: 'ISFJ', value: '97' },
+    { label: 'ISFP', value: '98' },
+    { label: 'INTJ', value: '99' },
+    { label: 'INTP', value: '100' },
+    { label: 'INFJ', value: '101' },
+    { label: 'INFP', value: '102' },
+    { label: 'ESTJ', value: '103' },
+    { label: 'ESTP', value: '104' },
+    { label: 'ESFJ', value: '105' },
+    { label: 'ESFP', value: '106' },
+    { label: 'ENTJ', value: '107' },
+    { label: 'ENTP', value: '108' },
+    { label: 'ENFJ', value: '109' },
+    { label: 'ENFP', value: '110' },
   ];
   return (
     <Background>
@@ -122,40 +184,58 @@ const ChooseInterest = () => {
         <div>
           <div className="scroll">
             <InterestMenu>
-              <p className="first">world</p>
+              <p className="first">Knowledge</p>
               <Checkbox.Group
                 className="check"
-                options={world}
+                options={Knowledge}
                 onChange={onChange}
               />
-              <p className="font">sports</p>
+              <p className="font">Wellness</p>
               <Checkbox.Group
                 className="check"
-                options={sports}
+                options={Wellness}
                 onChange={onChange}
               />
-              <p className="font">tech</p>
+              <p className="font">Tech</p>
               <Checkbox.Group
                 className="check"
-                options={tech}
+                options={Tech}
                 onChange={onChange}
               />
-              <p className="font">art</p>
+              <p className="font">Languages</p>
               <Checkbox.Group
                 className="check"
-                options={art}
+                options={Languages}
                 onChange={onChange}
               />
-              <p className="font">entertainment</p>
+              <p className="font">Entertainment</p>
               <Checkbox.Group
                 className="check"
-                options={entertainment}
+                options={Entertainment}
                 onChange={onChange}
               />
-              <p className="font">language</p>
+              <p className="font">Life</p>
               <Checkbox.Group
                 className="check"
-                options={language}
+                options={Life}
+                onChange={onChange}
+              />
+              <p className="font">Sports</p>
+              <Checkbox.Group
+                className="check"
+                options={Sports}
+                onChange={onChange}
+              />
+              <p className="font">Arts</p>
+              <Checkbox.Group
+                className="check"
+                options={Arts}
+                onChange={onChange}
+              />
+              <p className="font">WorldAffairs</p>
+              <Checkbox.Group
+                className="check"
+                options={WorldAffairs}
                 onChange={onChange}
               />
               <p className="font">MBTI</p>
