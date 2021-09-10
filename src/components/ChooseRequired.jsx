e4259811020752d154296543b56197a456d15850
Hee-jin506 <59636609+Hee-jin506@users.noreply.github.com>
2021-09-10 14:14:28 +0900

    add : purple background

--- src/components/ChooseRequired.jsx
+++ src/components/ChooseRequired.jsx
@@ -227,6 +227,7 @@ const SignUpForm = styled.div`
 `;
 
 const Background = styled.div`
+  background: linear-gradient(to bottom, #480088 0%, #0e032c 100%);
   width: 100%;
   height: 100%;
   .background-left {
