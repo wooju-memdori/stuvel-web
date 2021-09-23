b27c19434d9942cedeb0e314366ff76fac1ac4fc
Hayeon Kim <hayeon17kim@gmail.com>
2021-09-23 20:37:51 +0900

    update : useRecoilValueLoadable to useRecoilValue

--- src/components/Follow/Friends.jsx
+++ src/components/Follow/Friends.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useRecoilValueLoadable } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { Tabs, Input } from 'antd';
 import UserList from './UserList';
 
@@ -9,8 +9,8 @@ const { TabPane } = Tabs;
 const { Search } = Input;
 
 export default function Friends() {
-  const followers = useRecoilValueLoadable(followersState());
-  const followings = useRecoilValueLoadable(followingsState());
+  const followers = useRecoilValue(followersState);
+  const followings = useRecoilValue(followingsState);
   const onSearch = (value) => console.log(value);
 
   return (
@@ -21,13 +21,13 @@ export default function Friends() {
         onSearch={onSearch}
         style={{ marginBottom: '1rem' }}
       />
-      {followers.state === 'hasValue' && followings.state === 'hasValue' && (
+      {followers && followings && (
         <Tabs>
-          <TabPane tab={`팔로워(${followers.contents.length}명)`} key="1">
-            <UserList list={followers.contents} />
+          <TabPane tab={`팔로워(${followers.length}명)`} key="1">
+            <UserList list={followers} />
           </TabPane>
-          <TabPane tab={`팔로잉(${followings.contents.length}명)`} key="2">
-            <UserList list={followings.contents} />
+          <TabPane tab={`팔로잉(${followings.length}명)`} key="2">
+            <UserList list={followings} />
           </TabPane>
           <TabPane tab="나와 비행한 여행자" key="3">
             Coming Soon
