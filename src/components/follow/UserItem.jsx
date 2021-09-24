77cd3644de2d353a337aa29a22df343dcad6fd4a
Hayeon Kim <hayeon17kim@gmail.com>
2021-09-24 20:41:03 +0900

    refactor : extract popover from userItem component

--- src/components/follow/UserItem.jsx
+++ src/components/follow/UserItem.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { List, Popover } from 'antd';
-import { EllipsisOutlined } from '@ant-design/icons';
+import { List } from 'antd';
 import { number, shape, string, bool } from 'prop-types';
 import { useSetRecoilState } from 'recoil';
 
@@ -8,28 +7,18 @@ import axios from '../../utils/axios';
 import { forceUserListUpdate } from '../../state/follow';
 
 import UserProfile from './UserProfile';
+import UserPopover from './UserPopover';
 import { ChatSmallIcon, PlusIcon } from '../common/Icon';
 
 export default function UserListItem({ item }) {
   const userListUpdate = useSetRecoilState(forceUserListUpdate);
-  const forceUpdate = () => userListUpdate((n) => n + 1);
+  const userListRefresh = () => userListUpdate((n) => n + 1);
 
   const onClickFollow = (id) => () => {
     axios
       .post(`${process.env.REACT_APP_API_URL}/follow/${id}`)
       .then(() => {
-        forceUpdate();
-      })
-      .catch((error) => {
-        alert(error);
-      });
-  };
-
-  const onClickUnfollow = (id) => () => {
-    axios
-      .delete(`${process.env.REACT_APP_API_URL}/follow/${id}`)
-      .then(() => {
-        forceUpdate(id);
+        userListRefresh();
       })
       .catch((error) => {
         alert(error);
@@ -44,27 +33,7 @@ export default function UserListItem({ item }) {
         onClick={onClickFollow(item.id)}
       />
       <ChatSmallIcon style={{ marginRight: '0.8em' }} />
-      <Popover
-        style={{ borderRadius: 0 }}
-        placement="bottomRight"
-        content={
-          <List split={false} size="small">
-            {item.roomId && <List.Item>비행 따라가기</List.Item>}
-            <List.Item
-              onClick={onClickUnfollow(item.id)}
-              style={{ cursor: 'pointer' }}
-            >
-              언팔로우
-            </List.Item>
-            <List.Item>차단</List.Item>
-            <List.Item>신고</List.Item>
-            <List.Item>초대</List.Item>
-          </List>
-        }
-        trigger="click"
-      >
-        <EllipsisOutlined style={{ fontSize: '2.5em' }} />
-      </Popover>
+      <UserPopover item={item} userListRefresh={userListRefresh} />
     </List.Item>
   );
 }
---
+++ src/components/follow/UserPopover.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { List, Popover } from 'antd';
+import { EllipsisOutlined } from '@ant-design/icons';
+import { number, shape, string, bool, func } from 'prop-types';
+
+import axios from '../../utils/axios';
+
+export default function UserPopover({ item, userListRefresh }) {
+  const onClickUnfollow = (id) => () => {
+    axios
+      .delete(`${process.env.REACT_APP_API_URL}/follow/${id}`)
+      .then(() => {
+        userListRefresh(id);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+
+  return (
+    <Popover
+      style={{ borderRadius: 0 }}
+      placement="bottomRight"
+      content={
+        <List split={false} size="small">
+          {item.roomId && <List.Item>비행 따라가기</List.Item>}
+          <List.Item
+            onClick={onClickUnfollow(item.id)}
+            style={{ cursor: 'pointer' }}
+          >
+            언팔로우
+          </List.Item>
+          <List.Item>차단</List.Item>
+          <List.Item>신고</List.Item>
+          <List.Item>초대</List.Item>
+        </List>
+      }
+      trigger="click"
+    >
+      <EllipsisOutlined style={{ fontSize: '2.5em' }} />
+    </Popover>
+  );
+}
+
+UserPopover.propTypes = {
+  item: shape({
+    id: number,
+    nickname: string,
+    image: string,
+    roomId: string,
+    following: bool,
+  }).isRequired,
+  userListRefresh: func.isRequired,
+};
