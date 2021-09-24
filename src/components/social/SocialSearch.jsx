e8d6600ef84340d43e6bca5d0a5e484c528831aa
Hayeon Kim <hayeon17kim@gmail.com>
2021-09-25 00:03:33 +0900

    add : search input style

--- src/components/social/SocialSearch.jsx
+++ src/components/social/SocialSearch.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Input } from 'antd';
+import styled from 'styled-components';
+
+import { SearchIcon } from './Icons';
 
 const { Search } = Input;
 
@@ -7,11 +10,35 @@ export default function Social() {
   const onSearch = (value) => console.log(value);
 
   return (
-    <Search
-      placeholder="input search text"
+    <StyledSearch
+      placeholder="사용자 입력"
       allowClear
       onSearch={onSearch}
-      style={{ marginBottom: '1rem' }}
+      enterButton={<SearchIcon />}
+      style={{
+        marginBottom: '0.5rem',
+        backgroundColor: 'rgba(255, 255, 255, 0.14)',
+        color: 'rgba(255, 255, 255, 0.5)',
+      }}
     />
   );
 }
+const StyledSearch = styled(Search)`
+  .ant-input-wrapper {
+    border: 1px solid white;
+    border-radius: 8px;
+  }
+  .ant-input-affix-wrapper {
+    height: 2.6rem;
+    border: 0;
+  }
+  .ant-input-group-addon {
+    background-color: transparent;
+    border: 0;
+  }
+  .ant-input-search-button {
+    background-color: transparent;
+    border: 0;
+    padding: 0.8rem;
+  }
+`;
